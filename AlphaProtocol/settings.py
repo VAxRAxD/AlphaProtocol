7fa1188f07e7d41b6c31b6e77efc05bc44a19224
Varad Prabhu <varadp763@gmail.com>
2022-09-03 20:15:38 +0530

    Configured Staticfile settings

--- AlphaProtocol/settings.py
+++ AlphaProtocol/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR/'staticfile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
