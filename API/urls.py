5a90e7f05651c0fdd8afcf91ca958f5e32682f15
Varad Prabhu <varadp763@gmail.com>
2022-09-03 23:01:46 +0530

    Added image routes

--- API/urls.py
+++ API/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('genotp',views.genOtp,name="genOtp"),
     path('verotp/<str:code>/',views.verOtp,name="verOtp"),
     path('getotp',views.getcode,name="code"),
+    path('getimg',views.getImg,name="getImg"),
 ]
--- API/views.py
+++ API/views.py
@@ -16,11 +16,11 @@ def getRoutes(request):
 @api_view(['GET'])
 def genOtp(request):
     otp=f"{random.randint(10,99)}{random.choice(string.ascii_letters)}{random.randint(1,3)}"
-    cache.set('otp',otp,30)
+    cache.set('otp',otp,120)
     data=[otp]
     return Response(data)
 
-@api_view(['POST'])
+@api_view(['GET'])
 def verOtp(request,code):
     otp=cache.get('otp')
     if otp==code:
@@ -28,6 +28,15 @@ def verOtp(request,code):
         return Response(status=status.HTTP_200_OK)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def getImg(request):
+    data=[
+        'https://res.cloudinary.com/docvlyucw/image/upload/v1662212883/Alpha%20Protocol/StoryLine_1/Intro.png',
+        'https://res.cloudinary.com/docvlyucw/image/upload/v1662212887/Alpha%20Protocol/StoryLine_1/Story1Level1.png',
+        'https://res.cloudinary.com/docvlyucw/image/upload/v1662212890/Alpha%20Protocol/StoryLine_1/Story1Level2.png',
+    ]
+    return Response(data)
 
 def getcode(request):
     otp=cache.get('otp')
