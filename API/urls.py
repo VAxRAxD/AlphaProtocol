af9914a64cb422abb173135c97e1630b9fec976e
Varad Prabhu <varadp763@gmail.com>
2022-09-05 20:36:46 +0530

    Updated REST API

--- API/urls.py
+++ API/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getRoutes,name="getroutes"),
-    path('genotp',views.genOtp,name="genOtp"),
+    path('',views.getRoutes,name="getRoutes"),
+    path('genotp/<str:phone>',views.genOtp,name="genOtp"),
     path('verotp/<str:code>/',views.verOtp,name="verOtp"),
     path('getotp',views.getcode,name="code"),
-    path('getimg',views.getImg,name="getImg"),
+    path('getintro',views.getIntro,name="getIntro"),
+    path('getimg/<str:story>/',views.getImg,name="getImg"),
 ]
--- API/views.py
+++ API/views.py
@@ -1,4 +1,4 @@
-import random, string
+import random, string, smtplib
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,17 +8,33 @@ from django.core.cache import cache
 @api_view(['GET'])
 def getRoutes(request):
     routes=[
-        'GET/ap/genotp',
-        'GET/ap/verotp/:otp'
+        'GET/ap/genotp/',
+        'GET/ap/verotp/:otp',
+        'GET/ap/getintro',
+        'GET/ap/getimg/:story'
     ]
     return Response(routes)
 
 @api_view(['GET'])
-def genOtp(request):
+def genOtp(request,phone):
     otp=f"{random.randint(10,99)}{random.choice(string.ascii_letters)}{random.randint(1,3)}"
     cache.set('otp',otp,120)
-    data=[otp]
-    return Response(data)
+    CARRIERS = {
+    "att": "@mms.att.net",
+    "tmobile": "@tmomail.net",
+    "verizon": "@vtex.com",
+    "sprint": "@page.nextel.com"
+    }
+    EMAIL = "mastermindwebservice@gmail.com"
+    PASSWORD = "wearemastermindwebdevelopers763"
+    recipient = phone
+    auth = (EMAIL, PASSWORD)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(auth[0], auth[1])
+ 
+    server.sendmail(auth[0], recipient, otp)
+    return Response(status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def verOtp(request,code):
@@ -30,11 +46,23 @@ def verOtp(request,code):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
-def getImg(request):
+def getIntro(request):
     data=[
-        'https://res.cloudinary.com/docvlyucw/image/upload/v1662212883/Alpha%20Protocol/StoryLine_1/Intro.png',
-        'https://res.cloudinary.com/docvlyucw/image/upload/v1662212887/Alpha%20Protocol/StoryLine_1/Story1Level1.png',
-        'https://res.cloudinary.com/docvlyucw/image/upload/v1662212890/Alpha%20Protocol/StoryLine_1/Story1Level2.png',
+        'https://res.cloudinary.com/docvlyucw/image/upload/v1662390258/Alpha%20Protocol/Intro/Intro.jpg'
+    ]
+    return Response(data)
+
+@api_view(['GET'])
+def getImg(request,story):
+    data=[
+        {
+            'img':f'https://res.cloudinary.com/docvlyucw/image/upload/v1662212887/Alpha%20Protocol/StoryLine_{story}/Level1.png',
+            'ans':f'Story{story}Leve1'
+        },
+        {
+            'img':f'https://res.cloudinary.com/docvlyucw/image/upload/v1662212890/Alpha%20Protocol/StoryLine_{story}/Level2.png',
+            'ans':f'Story{story}Level2'
+        }
     ]
     return Response(data)
 
