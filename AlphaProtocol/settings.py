933004a0caec5ade12a6464602534bd8ae8edb38
Varad Prabhu <varadp763@gmail.com>
2022-09-08 18:17:13 +0530

    Favicon

--- AlphaProtocol/settings.py
+++ AlphaProtocol/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR/'staticfiles/'
+STATIC_ROOT=BASE_DIR/'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR/'static/'
+    BASE_DIR/'static'
 ]
 
 # Default primary key field type
