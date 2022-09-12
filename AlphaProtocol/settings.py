4cc82272d5547f0dc368f864e5dfdd6a4c6bb799
Varad Prabhu <varadp763@gmail.com>
2022-09-12 18:44:44 +0530

    Bug fixes

--- AlphaProtocol/settings.py
+++ AlphaProtocol/settings.py
@@ -92,9 +92,6 @@ DATABASES = {
     }
 }
 
-db_from_env=dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
