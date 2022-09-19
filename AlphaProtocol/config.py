b56809a245d57c896af118d1a44d9f319e182028
Varad Prabhu <varadp763@gmail.com>
2022-09-19 22:14:28 +0530

    Config Vars

--- AlphaProtocol/config.py
+++ AlphaProtocol/config.py
@@ -3,8 +3,5 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-EMAIL="varadprabhu@student.sfit.ac.in"
-PASSWORD="codewizard763"
-# PREFIX_URL=env('PREFIX_URL')
-# EMAIL=env('EMAIL')
-# PASSWORD=env('PASSWORD')
+EMAIL=env('EMAIL')
+PASSWORD=env('PASSWORD')
