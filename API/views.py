99c28bfa3a892e6b1eaccfc5f76207c7a7715a81
Varad Prabhu <varadp763@gmail.com>
2023-02-06 23:35:05 +0530

    Internal server error

--- API/views.py
+++ API/views.py
@@ -101,7 +101,7 @@ def elmVerOtp(request):
                 # data.append({"img" : f"https://res.cloudinary.com/docvlyucw/image/upload/v1675617827/Iris%202023/Day%201/Ending/end.png" })
             # combinations.append([{'data':data}])
             combinations.append(data)
-        return Response(combinations[indx-1])
+        return Response(combinations[indx-1],safe=False)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
